ed8c1ab7f203bcb63943bfdbdab474e44210ba41
Hirasawa Yui <ayachinenesanobauxichi@gmail.com>
2025-05-08 13:38:14 +0800

    click event

--- packages/components/mc-button/mc-button.vue
+++ packages/components/mc-button/mc-button.vue
@@ -14,14 +14,16 @@
       'is-loading': loading,
       'is-disabled': disabled,
     }"
+    @click="(e: MouseEvent) => useThrottle ? handleBtnClickThrottle(e) : handleBtnClick(e)"
   >
     <slot></slot>
   </component>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
-import type { ButtonProps } from "./types";
+import type { ButtonProps, ButtonEmits, ButtonInstance } from "./types";
+import { ref, toRefs } from "vue";
+import { throttle } from "lodash-es";
 
 // options
 defineOptions({
@@ -32,13 +34,29 @@ defineOptions({
 const props = withDefaults(defineProps<ButtonProps>(), {
   tag: "button",
   nativeType: "button",
+  useThrottle: true,
+  throttleDuration: 500,
 });
+const { throttleDuration } = toRefs(props);
+
+// emits
+const emit = defineEmits<ButtonEmits>();
 
 // slots
 const slots = defineSlots();
 
 // ref
 const _ref = ref<HTMLButtonElement>();
+
+// click event
+const handleBtnClick = (e: MouseEvent) => emit("click", e);
+// click event throttle
+const handleBtnClickThrottle = throttle(handleBtnClick, throttleDuration.value);
+
+// expose
+defineExpose<ButtonInstance>({
+  ref: _ref,
+});
 </script>
 
 <style scoped></style>
--- packages/components/mc-button/types.ts
+++ packages/components/mc-button/types.ts
@@ -1,3 +1,5 @@
+import type { Ref } from "vue";
+
 export type ButtonType = "primary" | "success" | "warning" | "danger" | "info";
 export type NativeType = "button" | "submit" | "reset";
 export type ButtonSize = "large" | "default" | "small";
@@ -13,4 +15,15 @@ export interface ButtonProps {
   circle?: boolean;
   plain?: boolean;
   round?: boolean;
+  autofocus?: boolean;
+  useThrottle?: boolean;
+  throttleDuration?: number;
+}
+
+export interface ButtonEmits {
+  (e: "click", val: MouseEvent): void;
+}
+
+export interface ButtonInstance {
+  ref: Ref<HTMLElement | void>;
 }
