56751acb3ff704a40801de646dabcb420ef1259a
lai.jiang <lai.jiang@ctx.sg>
2025-06-04 14:18:30 +0800

    drawer base

---
+++ packages/components/mc-drawer/mc-drawer.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="mc-drawer">mc drawer</div>
+</template>
+
+<script setup lang="ts">
+// options
+defineOptions({ name: "McDrawer", inheritAttrs: false });
+</script>
+
+<style scoped lang="scss">
+@use "./index.scss";
+</style>
