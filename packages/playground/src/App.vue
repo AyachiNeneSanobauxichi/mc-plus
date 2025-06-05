86b9033e230d3db87b9b2f77ed24cb869cb4f9a8
lai.jiang <lai.jiang@ctx.sg>
2025-06-05 11:47:35 +0800

    export mc success icon width && height

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,12 +1,56 @@
 <template>
   <div>
     <div id="drawer"></div>
-    <DrawerContainer />
+    <div><mc-success-icon /></div>
+    <div>
+      <mc-step
+        :model-value="step"
+        :steps="stepConfig"
+        type="horizontal"
+      ></mc-step>
+    </div>
+    <div>
+      <mc-button @click="handleNextStep">Next Step</mc-button>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import DrawerContainer from "./views/drawer-container.vue";
+import { ref } from "vue";
+import McSuccessIcon from "../../components/mc-success-icon/mc-success-icon.vue";
+import { McStep, McButton, type StepItem } from "mc-plus";
+
+const step = ref<number>(1);
+
+const stepConfig = ref<StepItem[]>([
+  {
+    key: 1,
+    label: "Step 1",
+    desc: "Step 1 description",
+  },
+  {
+    key: 2,
+    label: "Step 2",
+    desc: "Step 2 description",
+  },
+  {
+    key: 3,
+    label: "Step 3",
+    desc: "Step 3 description",
+  },
+  {
+    key: 4,
+    label: "Step 4",
+    desc: "Step 4 description",
+  },
+]);
+
+const handleNextStep = () => {
+  step.value++;
+  if (step.value > stepConfig.value.length) {
+    step.value = 1;
+  }
+};
 </script>
 
 <style scoped lang="scss">
