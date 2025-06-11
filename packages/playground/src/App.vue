2f1269ce286cb344ee75a1702d42c0411518cc6f
lai.jiang <lai.jiang@ctx.sg>
2025-06-11 23:57:49 +0800

    lightbox base

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,20 +1,18 @@
 <template>
-  <div class="select-container">
-    <input-group-container></input-group-container>
+  <div>
+    <!-- <input-group-container></input-group-container> -->
+    <lightbox-container />
   </div>
 </template>
 
 <script setup lang="ts">
-import InputGroupContainer from "./views/input-group-container.vue";
+// import InputGroupContainer from "./views/input-group-container.vue";
+import LightboxContainer from "./views/lightbox-container.vue";
 </script>
 
 <style scoped lang="scss">
-.select-container {
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  width: 1000px;
-  height: 800px;
+body,
+html {
+  height: 100vh;
 }
 </style>
---
+++ packages/playground/src/views/lightbox-container.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="lightbox-container">
+    <mc-light-box :v-model="visible">
+      <div class="light-box-content">Light Box</div>
+    </mc-light-box>
+    <mc-button @click="handleChangeVisible">Change Visible</mc-button>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import McLightBox from "../../../components/mc-light-box/mc-light-box.vue";
+import { McButton } from "mc-plus";
+
+const visible = ref<boolean>(false);
+const handleChangeVisible = () => {
+  visible.value = !visible.value;
+};
+</script>
+
+<style scoped lang="scss">
+.light-box-content {
+  width: 100%;
+  height: 800px;
+  background-color: lightgreen;
+}
+</style>
