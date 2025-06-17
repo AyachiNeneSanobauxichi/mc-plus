145ae928eed2cb4c5107ee4a109525241872e241
lai.jiang <lai.jiang@ctx.sg>
2025-06-17 19:48:55 +0800

    mc radio remark

--- packages/playground/src/views/radio-container.vue
+++ packages/playground/src/views/radio-container.vue
@@ -1,30 +1,89 @@
 <template>
   <div class="radio-container">
+    <div class="display">
+      <div>FormState: {{ formState }}</div>
+      <div class="button-group">
+        <mc-button @click="handleValidate">Validate</mc-button>
+        <mc-button @click="handleClear">Clear</mc-button>
+      </div>
+    </div>
     <div>
-      <mc-radio-group v-model="stu">
-        <mc-radio value="yui" label="Hirasawa Yui"></mc-radio>
-        <mc-radio value="azusa" label="Nakano Azusa"></mc-radio>
-        <mc-radio value="mio" label="Akiyama Mio"></mc-radio>
-        <mc-radio value="ritsu" label="Tainaka Ritsu"></mc-radio>
-        <mc-radio value="mugi" label="Kotobuku Tsumugi"></mc-radio>
-      </mc-radio-group>
+      <mc-form :model="formState" ref="FormRef" :rules="rules">
+        <mc-form-item label="Student" prop="stu" required>
+          <mc-radio-group v-model="formState.stu">
+            <div class="radio">
+              <mc-radio
+                value="yui"
+                label="Hirasawa Yui"
+                remark="HirasawaYuiHirasawaYuiHirasawaYuiHirasawaYuiHirasawaYui"
+              ></mc-radio>
+            </div>
+            <div class="radio">
+              <mc-radio value="azusa" label="Nakano Azusa"></mc-radio>
+            </div>
+            <div class="radio">
+              <mc-radio value="mio" label="Akiyama Mio"></mc-radio>
+            </div>
+            <div class="radio">
+              <mc-radio value="ritsu" label="Tainaka Ritsu"></mc-radio>
+            </div>
+            <div class="radio">
+              <mc-radio value="mugi" label="Kotobuku Tsumugi"></mc-radio>
+            </div>
+          </mc-radio-group>
+        </mc-form-item>
+      </mc-form>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 import McRadioGroup from "../../../components/mc-radio/mc-radio-group.vue";
 import McRadio from "../../../components/mc-radio/mc-radio.vue";
+import McForm from "../../../components/mc-form/mc-form.vue";
+import McFormItem from "../../../components/mc-form/mc-form-item.vue";
+import { McButton } from "mc-plus";
+import type { FormRules } from "mc-plus";
 
-const stu = ref<string>("yui");
+type FormState = {
+  stu: string;
+};
+
+const formState = reactive<FormState>({
+  stu: "yui",
+});
+
+const rules: FormRules<FormState> = {
+  stu: [
+    {
+      required: true,
+      validator: (_, value, callback) => {
+        if (value === "mio") {
+          callback(new Error("You can`t select Mio"));
+        } else {
+          callback();
+        }
+      },
+    },
+  ],
+};
+
+const FormRef = ref<typeof McForm>();
+
+const handleValidate = () => {
+  FormRef.value!.validate();
+};
+
+const handleClear = () => {
+  FormRef.value!.clearValidate();
+};
 </script>
 
 <style scoped lang="scss">
 .radio-container {
   width: 1200px;
   height: 800px;
-  background-color: lightgreen;
   position: fixed;
   top: 50%;
   left: 50%;
@@ -32,5 +91,24 @@ const stu = ref<string>("yui");
   display: flex;
   align-items: center;
   justify-content: center;
+}
+
+.display {
+  width: 1200px;
+  position: fixed;
+  top: 200px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+
+.radio {
+  width: 300px;
+}
+
+.button-group {
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  gap: 16px;
 }
 </style>
