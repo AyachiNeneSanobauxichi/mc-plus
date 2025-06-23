7683afb3f488b6b41b1fac932511c26613bc8ae8
lai.jiang <lai.jiang@ctx.sg>
2025-06-23 17:36:23 +0800

    mc switch playground

--- packages/playground/src/views/mc-switch/playground-switch.vue
+++ packages/playground/src/views/mc-switch/playground-switch.vue
@@ -1,11 +1,19 @@
 <template>
   <div class="playground-switch">
-    <mc-switch></mc-switch>
+    <mc-switch
+      v-model="check"
+      on-text="on"
+      off-text="off"
+      label="Mc Switch"
+    ></mc-switch>
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
 import McSwitch from "../../../../components/mc-switch/mc-switch.vue";
+
+const check = ref<boolean>(false);
 </script>
 
 <style scoped lang="scss"></style>
