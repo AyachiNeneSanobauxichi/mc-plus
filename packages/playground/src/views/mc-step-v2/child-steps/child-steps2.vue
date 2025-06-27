11b3e6e27273d07d6069aeac109c4661f080abdb
AyachiNeneSanobauxichi <ayachinenesanobauxichi@gmail.com>
2025-06-27 05:02:57 +0000

    回退 'Pull Request !43 : 回退 'Pull Request !42 : mc step v2 set line height''

---
+++ packages/playground/src/views/mc-step-v2/child-steps/child-steps2.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="child-steps2">Child Steps 2</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.child-steps2 {
+  width: 100%;
+  height: 50px;
+  background-color: lightcoral;
+}
+</style>
---
+++ packages/playground/src/views/mc-step-v2/steps/step3.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="step3">Step3</div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+.step3 {
+  width: 100%;
+  height: 100px;
+  background-color: lightgreen;
+}
+</style>
