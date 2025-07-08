69600c6861055f274376a38c05126e70c08f9571
lai.jiang <lai.jiang@ctx.sg>
2025-07-08 10:45:50 +0800

    mc otp input

--- packages/components/mc-otp/mc-otp.vue
+++ packages/components/mc-otp/mc-otp.vue
@@ -1,10 +1,29 @@
 <template>
-  <div class="mc-otp">Mc OTP</div>
+  <div>code: {{ code }}</div>
+  <div class="mc-otp" :class="{ 'mc-otp-disabled': props.disabled }">
+    <div class="mc-otp-input-container">
+      <div class="mc-otp-input-item" v-for="index in props.length" :key="index">
+        <mc-input
+          v-model="code[index - 1]"
+          :placeholder="''"
+          width="40px"
+          height="40px"
+          :maxlength="1"
+          :ref="(el) => setRef(index - 1, el as unknown as InputInstance)"
+          @input="handleInput(index - 1, $event)"
+        />
+      </div>
+    </div>
+    <mc-icon name="Accept_02" :size="24" />
+  </div>
 </template>
 
 <script setup lang="ts">
 import type { OtpProps, OtpEmits } from "./types";
-import { watchEffect } from "vue";
+import type { InputInstance } from "../mc-input";
+import { reactive, ref, watchEffect } from "vue";
+import McIcon from "../mc-icon/mc-icon.vue";
+import McInput from "../mc-input/mc-input.vue";
 
 // options
 defineOptions({ name: "McOtp" });
@@ -17,6 +36,26 @@ const props = withDefaults(defineProps<OtpProps>(), {
 
 // emits
 const emits = defineEmits<OtpEmits>();
+
+// refs
+const inputRefs = ref<InputInstance[]>([]);
+
+// set ref
+const setRef = (index: number, el: InputInstance) => {
+  (inputRefs.value as unknown as InputInstance[])[index] = el;
+};
+
+// code
+const code = reactive(new Array(props.length).fill(""));
+
+// input
+const handleInput = (index: number, value: string) => {
+  code[index] = value;
+
+  if (value && index < props.length - 1) {
+    inputRefs.value[index + 1]?.focus();
+  }
+};
 
 // model value length check
 watchEffect(() => {
