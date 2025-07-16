593a94c79b0429f7c3e6b46c80a4e172cbe9e26b
lai.jiang <lai.jiang@ctx.sg>
2025-07-16 18:27:47 +0800

    mc select group plus base style

--- packages/components/mc-select-plus/mc-select-group-plus.vue
+++ packages/components/mc-select-plus/mc-select-group-plus.vue
@@ -1,10 +1,26 @@
 <template>
-  <div class="mc-select-group"></div>
+  <ul v-show="isVisible" class="mc-select-group">
+    <li class="mc-select-group-label">{{ label }}</li>
+    <li>
+      <ul class="mc-select-option-list">
+        <slot></slot>
+      </ul>
+    </li>
+  </ul>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+import type { SelectGroupPlusProps } from "./types";
+
 // options
 defineOptions({ name: "McSelectGroup" });
+
+// props
+const props = defineProps<SelectGroupPlusProps>();
+
+// visible
+const isVisible = ref<boolean>(true);
 </script>
 
 <style scoped lang="scss">
