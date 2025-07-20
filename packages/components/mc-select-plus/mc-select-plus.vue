12aaf8cf18b7acdf64a54632dacdb9f6c8b137c2
Hirasawa Yui <ayachinenesanobauxichi@gmail.com>
2025-07-20 23:02:55 +0800

    mc select plus fix error icon not display

--- packages/components/mc-select-plus/mc-select-plus.vue
+++ packages/components/mc-select-plus/mc-select-plus.vue
@@ -218,7 +218,7 @@ const props = withDefaults(defineProps<SelectPlusProps>(), {
   showHeader: false,
   showFooter: false,
   placement: "bottom-start",
-  formValidate: true,
+  formValidateStyle: true,
 });
 
 // emits
--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="playground">
     <playground-input-group></playground-input-group>
+    <!-- <playground-select-plus></playground-select-plus> -->
   </div>
 </template>
 
 <script setup lang="ts">
 import PlaygroundInputGroup from "./views/mc-input-group/playground-input-group.vue";
+// import PlaygroundSelectPlus from "./views/mc-select-plus/playground-select-plus.vue";
 </script>
 
 <style scoped lang="scss">
