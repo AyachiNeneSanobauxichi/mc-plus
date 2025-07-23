59b20be1213791c09bae219e022be9d540082898
Hirasawa Yui <ayachinenesanobauxichi@gmail.com>
2025-07-24 00:00:52 +0800

    mc lightbox test

--- packages/playground/src/App.vue
+++ packages/playground/src/App.vue
@@ -1,23 +1,25 @@
 <template>
   <div class="playground">
     <!-- <playground-input-group></playground-input-group> -->
-    <playground-select-plus></playground-select-plus>
+    <!-- <playground-select-plus></playground-select-plus> -->
     <!-- <playground-alert-message></playground-alert-message> -->
     <!-- <playground-toast></playground-toast> -->
     <!-- <playground-count></playground-count> -->
     <!-- <playground-reset></playground-reset> -->
     <!-- <playground-copy></playground-copy> -->
+    <playground-lightbox></playground-lightbox>
   </div>
 </template>
 
 <script setup lang="ts">
 // import PlaygroundInputGroup from "./views/mc-input-group/playground-input-group.vue";
-import PlaygroundSelectPlus from "./views/mc-select-plus/playground-select-plus.vue";
+// import PlaygroundSelectPlus from "./views/mc-select-plus/playground-select-plus.vue";
 // import PlaygroundAlertMessage from "./views/mc-alert-message/playground-alert-message.vue";
 // import PlaygroundToast from "./views/mc-toast/playground-toast.vue";
 // import PlaygroundCount from "./views/mc-count/playground-count.vue";
 // import PlaygroundReset from "./views/mc-reset/playground-reset.vue";
 // import PlaygroundCopy from "./views/mc-copy/playground-copy.vue";
+import PlaygroundLightbox from "./views/mc-lightbox/playground-lightbox.vue";
 </script>
 
 <style scoped lang="scss">
---
+++ packages/playground/src/views/mc-lightbox/playground-lightbox.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="playground-lightbox">
+    <mc-lightbox
+      v-model="visible"
+      title="Mc Lightbox"
+      size="large"
+    ></mc-lightbox>
+    <div class="tool-bar">
+      <mc-button @click="handleChangeVisible">Change Visible</mc-button>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import McLightbox from "../../../../components/mc-lightbox/mc-lightbox.vue";
+import { McButton } from "mc-plus";
+
+const visible = ref<boolean>(false);
+
+const handleChangeVisible = () => {
+  visible.value = !visible.value;
+};
+</script>
+
+<style scoped lang="scss">
+.playground-lightbox {
+  .tool-bar {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 320px;
+  }
+}
+</style>
