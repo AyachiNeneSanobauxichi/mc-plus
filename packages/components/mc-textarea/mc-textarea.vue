36ec3ede8961ba15458816c8c9ae091e202ace6e
lai.jiang <lai.jiang@ctx.sg>
2025-08-07 09:58:54 +0800

    mc textarea base

---
+++ packages/components/mc-textarea/mc-textarea.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="mc-textarea">
+    <textarea></textarea>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped lang="scss">
+@use "./index.scss";
+</style>
---
+++ packages/playground/src/views/mc-textarea/playground-textarea.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="playground-textarea">
+    <mc-textarea></mc-textarea>
+  </div>
+</template>
+
+<script setup lang="ts">
+import McTextarea from "../../../../components/mc-textarea/mc-textarea.vue";
+</script>
+
+<style scoped lang="scss">
+.playground-textarea {
+  width: 80%;
+  height: 800px;
+  margin: 0 auto;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+</style>
