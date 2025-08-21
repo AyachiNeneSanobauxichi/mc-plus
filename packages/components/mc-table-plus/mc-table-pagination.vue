56ffbd2b27a1352aa12765c4586933a40ed585e0
lai.jiang <lai.jiang@ctx.sg>
2025-08-21 17:47:14 +0800

    mc table pagination total page

--- packages/components/mc-table-plus/mc-table-pagination.vue
+++ packages/components/mc-table-plus/mc-table-pagination.vue
@@ -13,17 +13,22 @@
       <mc-icon name="Left-Chevron" />
     </li>
     <li class="mc-table-pagination-item mc-table-pagination-item-page">1</li>
-    <mc-table-ellipsis />
-    <li class="mc-table-pagination-item mc-table-pagination-item-page">4</li>
-    <li
+    <!-- <mc-table-ellipsis /> -->
+    <!-- <li class="mc-table-pagination-item mc-table-pagination-item-page">4</li> -->
+    <!-- <li
       class="mc-table-pagination-item mc-table-pagination-item-page"
       :class="{ 'mc-table-pagination-item-active': true }"
     >
       5
+    </li> -->
+    <!-- <li class="mc-table-pagination-item mc-table-pagination-item-page">6</li> -->
+    <!-- <mc-table-ellipsis /> -->
+    <li
+      class="mc-table-pagination-item mc-table-pagination-item-page"
+      v-if="totalPage && totalPage > 1"
+    >
+      {{ totalPage }}
     </li>
-    <li class="mc-table-pagination-item mc-table-pagination-item-page">6</li>
-    <mc-table-ellipsis />
-    <li class="mc-table-pagination-item mc-table-pagination-item-page">10</li>
     <li class="mc-table-pagination-item mc-table-pagination-item-next">
       <mc-icon name="Right-Chevron" />
     </li>
@@ -36,10 +41,21 @@
 <script setup lang="ts">
 import { MC_TABLE_PAGINATION } from "./constant";
 import McIcon from "../mc-icon/mc-icon.vue";
-import McTableEllipsis from "./mc-table-ellipsis.vue";
+// import McTableEllipsis from "./mc-table-ellipsis.vue";
+import { useTableContext } from "./hooks";
+import { computed } from "vue";
 
 // options
 defineOptions({ name: MC_TABLE_PAGINATION });
+
+// use table context
+const { pagination } = useTableContext();
+
+// total page
+const totalPage = computed(() => {
+  if (!pagination) return;
+  return Math.ceil(pagination.total / pagination.pageSize);
+});
 </script>
 
 <style scoped lang="scss">
