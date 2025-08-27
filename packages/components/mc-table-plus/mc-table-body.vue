bf71818733f6cf14d8e98bb88ca90ce418ce70ec
lai.jiang <lai.jiang@ctx.sg>
2025-08-27 11:10:53 +0800

    mc table loading

--- packages/components/mc-table-plus/mc-table-body.vue
+++ packages/components/mc-table-plus/mc-table-body.vue
@@ -1,7 +1,9 @@
 <template>
   <table class="mc-table-body">
     <div class="mc-table-body-loading" v-if="loading">
-      <slot name="loading">Loading...</slot>
+      <slot name="loading">
+        <mc-loading loading size="small" type="pulse" />
+      </slot>
     </div>
     <colgroup class="mc-table-body-colgroup">
       <col
@@ -43,8 +45,9 @@
 
 <script setup lang="ts">
 import type { McTablePlusBodyProps } from "./types";
-import { MC_TABLE_BODY } from "./constant";
+import McLoading from "../mc-loading/mc-loading.vue";
 import McTableBodyCell from "./mc-table-body-cell.vue";
+import { MC_TABLE_BODY } from "./constant";
 
 // options
 defineOptions({ name: MC_TABLE_BODY });
