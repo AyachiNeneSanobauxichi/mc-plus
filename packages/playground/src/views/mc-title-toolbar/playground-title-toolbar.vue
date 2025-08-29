380d04b5500f5d02cd7ce968f79cb6195930ace2
r_dilywang <dily.wang@ctx.sg>
2025-08-29 17:15:23 +0800

    mc_title_toolbar

---
+++ packages/playground/src/views/mc-title-toolbar/playground-title-toolbar.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="playground-title-toolbar">
+    <mc-title-toolbar :show-reset="true" :show-mandatory="true">
+      <template #reset>刷新</template>
+      <template #mandatory>必填</template>
+    </mc-title-toolbar>
+  </div>
+</template>
+
+<script setup lang="ts">
+import McTitleToolbar from "../../../../components/mc-title-toolbar/mc-title-toolbar.vue";
+</script>
+
+<style scoped lang="scss"></style>
