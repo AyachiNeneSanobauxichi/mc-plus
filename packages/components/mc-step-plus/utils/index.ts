22be14781c95d801cae324c8f4282da2a754140a
lai.jiang <lai.jiang@ctx.sg>
2025-09-01 18:06:46 +0800

    mc step remove parent context when has children

--- packages/components/mc-step-plus/utils/index.ts
+++ packages/components/mc-step-plus/utils/index.ts
@@ -1,5 +1,5 @@
 import type { Component, Slot, VNode, VNodeNormalizedChildren } from "vue";
-import type { McStepItem, McStepKey } from "../types";
+import type { McStepItem } from "../types";
 import { isArray, isFunction, isString } from "lodash-es";
 import { MC_STEP_CHILD_ITEM_PLUS, MC_STEP_ITEM_PLUS } from "../constant";
 
@@ -9,7 +9,7 @@ export const generateStepItems = (children?: VNodeNormalizedChildren) => {
 
   const flattenNodes = (
     children?: VNodeNormalizedChildren,
-    parentKey?: McStepKey
+    parentStep?: McStepItem
   ) => {
     if (!isArray(children)) return;
     (children as VNode[]).forEach((item) => {
@@ -23,24 +23,30 @@ export const generateStepItems = (children?: VNodeNormalizedChildren) => {
             ? item.children?.default()
             : item.children;
 
-        stepItems.push({
+        const currentStep: McStepItem = {
           step: item.props?.step,
           label: item.props?.label,
           desc: item.props?.desc,
-          parentKey: parentKey,
-        });
-        flattenNodes(_children, item.props?.step);
+          context: (item.children as { default?: () => Component })
+            ?.default as Slot,
+        };
+
+        stepItems.push(currentStep);
+        flattenNodes(_children, currentStep);
       } else if (name === MC_STEP_CHILD_ITEM_PLUS) {
+        if (parentStep) {
+          parentStep.context = void 0;
+        }
         stepItems.push({
           step: item.props?.step,
           label: item.props?.label,
           desc: item.props?.desc,
-          parentKey: parentKey,
+          parentKey: parentStep?.step,
           context: (item.children as { default?: () => Component })
             ?.default as Slot,
         });
       } else if (Array.isArray(item.children)) {
-        flattenNodes(item.children, parentKey);
+        flattenNodes(item.children, parentStep);
       }
     });
   };
