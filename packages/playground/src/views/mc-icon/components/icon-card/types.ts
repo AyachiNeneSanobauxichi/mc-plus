21a4c67326f1cf350c7973a89abf1c81756424d0
lai.jiang <lai.jiang@ctx.sg>
2025-09-04 15:03:02 +0800

    mc icon playground copy

---
+++ packages/playground/src/views/mc-icon/components/icon-card/types.ts
@@ -0,0 +1,5 @@
+import type { IconType } from "mc-plus";
+
+export interface IconCardProps {
+  icon: IconType;
+}
--- packages/playground/src/views/mc-icon/playground-icon.vue
+++ packages/playground/src/views/mc-icon/playground-icon.vue
@@ -1,19 +1,34 @@
 <template>
-  <div>
-    <mc-icon name="List" />
-    <mc-icon name="Grid" />
-    <mc-icon name="Indicator" />
-    <mc-icon name="Bullet" />
-    <mc-icon name="Loading" />
-    <mc-icon name="Accept_02" />
-    <mc-icon name="Accept" />
-    <mc-icon name="Sort_Asc" />
-    <mc-icon name="Sort_Desc" />
+  <div class="playground-icon">
+    <ul class="playground-icon-list">
+      <li class="playground-icon-item" v-for="icon in iconList" :key="icon">
+        <icon-card :icon="icon" />
+      </li>
+    </ul>
   </div>
 </template>
 
 <script setup lang="ts">
-import McIcon from "../../../../components/mc-icon/mc-icon.vue";
+import IconCard from "./components/icon-card/index.vue";
+import { useIcon } from "./hooks";
+
+// icon list
+const iconList = useIcon();
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+@use "@mc-plus/theme/mixins.scss" as mixin;
+
+.playground-icon {
+  .playground-icon-list {
+    @include mixin.reset-ul-style;
+    @include mixin.flex-center(row, flex-start, flex-start, 16px);
+    align-content: flex-start;
+    flex-wrap: wrap;
+
+    .playground-icon-item {
+      @include mixin.reset-li-style;
+    }
+  }
+}
+</style>
