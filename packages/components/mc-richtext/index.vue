5d5ac1c795b14fdcef302c1bde6ddc950ee33e34
lai.jiang <lai.jiang@ctx.sg>
2025-09-18 15:11:42 +0800

    mc richtext quill editor

--- packages/components/mc-richtext/index.vue
+++ packages/components/mc-richtext/index.vue
@@ -1,12 +1,26 @@
 <template>
-  <div class="mc-richtext">Mc Richtext</div>
+  <div class="mc-richtext">
+    <quill-editor theme="snow" :toolbar="toolbarOptions" />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { QuillEditor } from "@vueup/vue-quill";
 import { MC_RICHTEXT } from "./constant";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 // options
 defineOptions({ name: MC_RICHTEXT });
+
+// toolbar options
+const toolbarOptions = [
+  [{ header: 1 }, { header: 2 }],
+  ["bold", "italic"],
+  [{ indent: "-1" }, { indent: "+1" }],
+  [{ list: "bullet" }, { list: "ordered" }],
+  ["link", "image"],
+  ["clean"],
+];
 </script>
 
 <style scoped lang="scss">
