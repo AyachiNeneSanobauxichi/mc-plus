f17a725ae8cdd35a8c4180db9d5a421e87496799
lai.jiang <lai.jiang@ctx.sg>
2025-09-18 10:29:36 +0800

    mc richtext base

---
+++ packages/components/mc-richtext/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="mc-richtext">Mc Richtext</div>
+</template>
+
+<script setup lang="ts">
+import { MC_RICHTEXT } from "./constant";
+
+// options
+defineOptions({ name: MC_RICHTEXT });
+</script>
+
+<style scoped lang="scss">
+@use "./index.scss";
+</style>
---
+++ packages/playground/src/views/mc-plus/mc-richtext/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="playground-richtext">
+    <mc-richtext />
+  </div>
+</template>
+
+<script setup lang="ts">
+import McRichtext from "./../../../../../components/mc-richtext/index.vue";
+</script>
+
+<style scoped lang="scss"></style>
